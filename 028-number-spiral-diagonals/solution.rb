06141bee8c22c946882ee86864a37bd19a700fba
John Doe <johndoe@example.com>
2015-12-30 21:15:57 -0600

    Complete 28 solution and explanation

--- 028-number-spiral-diagonals/solution.rb
+++ 028-number-spiral-diagonals/solution.rb
@@ -12,33 +12,25 @@
 # What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the
 # same way?
 
-def print_nicely(some_2d_array)
-  some_2d_array.each {|row| print row; puts ""}
-end
+def diagonal_sum(spiral_grid_length)
+  increment = 2
+  numbers_to_sum = [1]
 
-a = [[21, 22, 23, 24, 25],
-    [20, 7, 8, 9, 10],
-    [19, 6, 1, 2, 11],
-    [18, 5, 4, 3, 12],
-    [17, 16, 15, 14, 13]]
-
-a_reversed = a.map {|x| x.reverse}
-
-def diagonal_sum(some_2d_array)
-  a = some_2d_array
-  a_rev = a.map {|x| x.reverse}
-  upper_left_coords = [0, 0]
-  upper_right_coords = [0, some_2d_array.length]
-  while some_2d_array[coords[0]][coords[1]] <= some_2d_array.first.length
-
+  until numbers_to_sum.last == spiral_grid_length**2
+    4.times { numbers_to_sum << numbers_to_sum.last + increment}
+    increment += 2
   end
 
+  numbers_to_sum.reduce(:+)
 end
 
-spiral_array = []
+p diagonal_sum(1001)
 
-1001.times {spiral_array << []}
-spiral_array.each {|empty_array| 1001.times {empty_array << []}}
+# => 669171001
 
-print_nicely(a_reversed)
-print_nicely(a)
+# I tried to solve this visually at first. That is, I wrote a method that finds the diagonal
+# sum of any square grid. I was about to place the numbers (1..1001**2) into an empty grid when
+# I realized that there is a very simple mathematical pattern to this problem. After 1, the
+# number of spaces we must move (in a spiraling movement pattern) before we reach the next
+# countable number goes like this: 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6... Once I realized this,
+# it was very easy for me to write a method that finds the diagonal sum of any spiraling grid.
