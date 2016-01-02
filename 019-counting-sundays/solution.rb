510cb05b073c354e20b7c39144e6e190dafbaccd
John Doe <johndoe@example.com>
2016-01-01 21:35:57 -0600

    Add working solution

--- 019-counting-sundays/solution.rb
+++ 019-counting-sundays/solution.rb
@@ -1,7 +1,5 @@
 def days_in(year)
-  if year % 4 != 0
-    365
-  elsif year % 400 == 0
+  if year % 400 == 0
     366
   elsif year % 100 == 0
     365
@@ -12,8 +10,35 @@ def days_in(year)
   end
 end
 
-p days_in_1900s = (1901..2000).inject(0) {|sum, year| sum + days_in(year)}
+leap_year_months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+normal_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def sundays_from(date_1, date_2) #[year, ]
+days_in_1900s = (1901..2000).inject(0) {|sum, year| sum + days_in(year)}
 
+# 1901 started on a Tuesday. We want SUNDAYS!
+
+sundays = []
+firsts_of_months = []
+sundays_on_firsts_of_months = 0
+
+(1..days_in_1900s).each {|day| sundays << ((day + 1) % 7 == 0)}
+
+(1901..2000).each_with_index do |year, index|
+  if days_in(year) == 366
+    leap_year_months.each do |days_in_month|
+      firsts_of_months << true
+      (days_in_month - 1).times { firsts_of_months << false}
+    end
+  else
+    normal_months.each do |days_in_month|
+      firsts_of_months << true
+      (days_in_month - 1).times { firsts_of_months << false}
+    end
+  end
 end
+
+(0...days_in_1900s).each do |day_index|
+  sundays_on_firsts_of_months += 1 if sundays[day_index] == true && firsts_of_months[day_index] == true
+end
+
+p sundays_on_firsts_of_months
