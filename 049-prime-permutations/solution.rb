d92610d9005f478d370c4a98afa8259be8e5d243
John Doe <johndoe@example.com>
2016-01-19 18:02:47 -0600

    Complete p49

--- 049-prime-permutations/solution.rb
+++ 049-prime-permutations/solution.rb
@@ -10,7 +10,6 @@
 require 'prime'
 
 class Integer
-
   def permutations
     self.to_s.split("").permutation.to_a.map {|subarr| subarr.join("").to_i}
   end
@@ -18,23 +17,9 @@ class Integer
   def prime_permutations
     self.permutations.select {|perm| perm.prime?}
   end
-
-  def self.four_digit_primes
-    Prime.take_while {|prime| prime < 10_000}.select {|prime| prime > 999}
-  end
-
-  def prime_permutation_of?(number)
-    self.prime_permutations.include?(number)
-  end
-
 end
 
 class Array
-
-  def repeating_elements
-    self.select {|elem| self.count(elem) > 1}.uniq
-  end
-
   def difference
     self[0] > self[1] ? self[0] - self[1] : self[1] - self[0]
   end
@@ -44,52 +29,31 @@ class Array
     self.combination(2).to_a.each do |combo|
       diffs[combo] = combo.difference
     end
-    diffs
-    # self.combination(2).to_a.map {|combo| combo.difference}
+    diffs.select {|k,v| v != 0}
   end
-
-  def has_two_common_differences?
-      self.differences.each do |diff|
-        return true if differences.count(diff) == 2
-      end
-    false
-  end
-
-  # def common_difference
-  #   diffs = self.differences
-  #   common_differences = []
-  #   diffs.each do |diff|
-  #     common_differences << diff if diffs.count(diff) == 2
-  #   end
-  #   common_differences
-  # end
-
 end
 
 class Hash
-
   def common_differences
-    common_values = self.values.repeating_elements
-
-    common_differences = {}
-
     self.select do |key, value|
-      # Find
+      self.select {|k,v| v == value}.keys.flatten.uniq.length == 3
     end
-
-    # self.select {|key, value| common_values.include?(value)}
   end
 
+  def special_concatenated_number
+    number = self.common_differences.keys.flatten.uniq.sort.join("")
+    number.length == 12 ? number : ""
+  end
 end
 
-p [1487, 1847, 4817, 4871, 8147, 8741, 7481, 7841].differences.common_differences
+counter = 1000
+concat_num = counter.prime_permutations.differences.special_concatenated_number
 
-# p 1487.prime_permutation_of?(4817)
+until concat_num != "" && concat_num != "148748178147"
+  counter += 1
+  concat_num = counter.prime_permutations.differences.special_concatenated_number
+end
 
-# p 1487.prime_permutations
+p concat_num.to_i
 
-# possibilities = Integer.four_digit_primes.select {|prime| prime.prime_permutations.has_two_common_differences?}
-
-# p possibilities.length
-
-# {number => common_difference} make it a hash?
+# => 296962999629
