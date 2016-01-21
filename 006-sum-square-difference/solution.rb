7d5232ca5a856c1117c302249587fe18a91f2cf8
John Doe <johndoe@example.com>
2016-01-20 18:15:09 -0600

    Add timer

--- 006-sum-square-difference/solution.rb
+++ 006-sum-square-difference/solution.rb
@@ -12,10 +12,13 @@
 # the first one hundred natural numbers and the square
 # of the sum.
 
+start_time = Time.now
+
 sum_of_squares = (1..100).inject {|sum, int| sum + int ** 2 }
 square_of_sum = (1..100).reduce(:+) ** 2
 
 p square_of_sum - sum_of_squares
+p "calculated in #{(Time.now - start_time) * 1000} ms"
 
 # => 25164150
 
