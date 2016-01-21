b1fb73be773f459275ebaf9c4f5287872ef2d465
John Doe <johndoe@example.com>
2016-01-20 18:00:40 -0600

    Add timer

--- 001-mults-of-3-and-5/solution.rb
+++ 001-mults-of-3-and-5/solution.rb
@@ -1,6 +1,9 @@
+start_time = Time.now
+
 multiples = []
 (1...1000).each do |int|
   multiples << int if int % 3 == 0 || int % 5 == 0
 end
 
 p multiples.reduce(:+)
+p "calculated in #{(Time.now - start_time) * 1000} ms"
