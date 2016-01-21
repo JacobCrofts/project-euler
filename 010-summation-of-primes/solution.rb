6209b2c0e5e718dbfb2b1923eae4140c37c191c8
John Doe <johndoe@example.com>
2016-01-20 18:33:55 -0600

    Add timer and refactor for simplicity

--- 010-summation-of-primes/solution.rb
+++ 010-summation-of-primes/solution.rb
@@ -2,10 +2,11 @@
 
 # Find the sum of all the primes below two million.
 
+start_time = Time.now
+
 require 'prime'
 
-solution = Prime.each(2_000_000).reduce(:+)
-
-p solution
+p Prime.each(2_000_000).reduce(:+)
+p "calculated in #{(Time.now - start_time) * 1000} ms"
 
 # => 142913828922
