39458ca64a483c4dd8cab01943f1a6778a84eca8
John Doe <johndoe@example.com>
2016-02-15 18:35:32 -0600

    Finish logical process for 29/js

---
+++ 029-distinct-powers/javascript-solution.js
@@ -0,0 +1,23 @@
+exponentiateStrings = function(string1, string2) {
+  // must have a powers function for strings because 100 ** 100 is too high for JS...
+}
+
+// var powers = [];
+
+// for (var a = 2; a <= 100; a++) {
+//   for (var b = 2; b <= 100; b++) {
+//     powers.push(exponentiateStrings(a.toString(), b.toString()));
+//   }
+// }
+
+// var distinctPowers = [];
+// var usedPowers = {};
+
+// powers.forEach(function(power) {
+//   if (!usedPowers[power]) {
+//     distinctPowers.push(power);
+//   }
+//   usedPowers[power] = true;
+// })
+
+// console.log(distinctPowers.length);
